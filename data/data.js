2e8ea2a9302f615164e6bf21aee053f9c56fef70
pankajgouraw <pgouraw@gmail.com>
2019-08-12 16:29:24 +0530

    mcq suggestion with picture

---
+++ data/data.js
@@ -0,0 +1,110 @@
+
+const bgColor = '#527549';
+const borderColor = '10px solid rgb(28, 151, 85)';
+const header = 'MCQ';
+let questPerPage = 5;
+const data = [
+	{
+		'question' : 'Which animal in this video1?',
+		'video'    : 'img/bear.jpg',
+		'corrAns'  : 'Bear',
+		'sugg1'    : 'Tiger',
+		'sugg2'    : 'Elephant',
+		'sugg3'    : 'Zebra'
+	},
+
+	{
+		'question' : 'Which animal in this video2?',
+		'video'    : 'img/dog.jpg',
+		'corrAns'  : 'Dog',
+		'sugg1'    : 'Lion',
+		'sugg2'    : 'Elephant',
+		'sugg3'    : 'Zebra'
+	},
+
+	{
+		'question' : 'Which animal in this video3?',
+		'video'    : 'img/cat.jpg',
+		'corrAns'  : 'Cat',
+		'sugg1'    : 'Tiger',
+		'sugg2'    : 'Dog',
+		'sugg3'    : 'Zebra'
+	},
+
+	{
+		'question' : 'Which animal in this video4?',
+		'video'    : 'img/cow.jpg',
+		'corrAns'  : 'Cow',
+		'sugg1'    : 'Cat',
+		'sugg2'    : 'Elephant',
+		'sugg3'    : 'Zebra'
+	},
+
+	{
+		'question' : 'Which animal in this video5?',
+		'video'    : 'img/bear.jpg',
+		'corrAns'  : 'Bear',
+		'sugg1'    : 'Tiger',
+		'sugg2'    : 'Elephant',
+		'sugg3'    : 'Zebra'
+	},
+
+		{
+		'question' : 'Which animal in this video6?',
+		'video'    : 'img/bear.jpg',
+		'corrAns'  : 'Bear',
+		'sugg1'    : 'Tiger',
+		'sugg2'    : 'Elephant',
+		'sugg3'    : 'Zebra'
+	},
+		{
+		'question' : 'Which animal in this video7?',
+		'video'    : 'img/cow.jpg',
+		'corrAns'  : 'Cow',
+		'sugg1'    : 'Cat',
+		'sugg2'    : 'Elephant',
+		'sugg3'    : 'Zebra'
+	},
+		{
+		'question' : 'Which animal in this video8?',
+		'video'    : 'img/bear.jpg',
+		'corrAns'  : 'Bear',
+		'sugg1'    : 'Tiger',
+		'sugg2'    : 'Elephant',
+		'sugg3'    : 'Zebra'
+	},
+
+		{
+		'question' : 'Which animal in this video9?',
+		'video'    : 'img/bear.jpg',
+		'corrAns'  : 'Bear',
+		'sugg1'    : 'Tiger',
+		'sugg2'    : 'Elephant',
+		'sugg3'    : 'Zebra'
+	},
+		{
+		'question' : 'Which animal in this video10?',
+		'video'    : 'img/cow.jpg',
+		'corrAns'  : 'Cow',
+		'sugg1'    : 'Cat',
+		'sugg2'    : 'Elephant',
+		'sugg3'    : 'Zebra'
+	},
+
+	{
+		'question' : 'Which animal in this video11?',
+		'video'    : 'img/dog.jpg',
+		'corrAns'  : 'Dog',
+		'sugg1'    : 'Cat',
+		'sugg2'    : 'Elephant',
+		'sugg3'    : 'Zebra'
+	},
+		{
+		'question' : 'Which animal in this video12?',
+		'video'    : 'img/dog.jpg',
+		'corrAns'  : 'Dog',
+		'sugg1'    : 'Cat',
+		'sugg2'    : 'Elephant',
+		'sugg3'    : 'Zebra'
+	}
+];
